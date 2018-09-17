526cd1e4808dcb98b56b79ec30e45ee0831401d2
장우석 <cyj2651@naver.com>
2018-09-17 17:25:11 +0900

    drill_05 이미지 변경, 움직이는 함수 생성
    좌우로 움직이는 이미지를 위해 png 파일 변경
    - 움직이는 함수를 만들었지만 실행이 안됨

--- Drills/Drill_05/drill_05.py
+++ Drills/Drill_05/drill_05.py
@@ -3,48 +3,65 @@ from pico2d import *
 open_canvas()
 
 grass = load_image('grass.png')
-character = load_image('character.png')
+character = load_image('animation_sheet.png')
 
 # 2
 
 count = 0
 x, y = 0, 0
-def save_coord():
+b_x, b_y = 0, 0
+def save_coord(x, y):
     if(count == 0):
         x, y = 203, 535
-    if(count == 0):
+    elif(count == 1):
+        b_x, b_y = x, y
         x, y = 132, 243
-    if(count == 0):
+    elif(count == 2):
+        b_x, b_y = x, y
         x, y = 535, 470
-    if(count == 0):
+    elif(count == 3):
+        b_x, b_y = x, y
         x, y = 477, 203
-    if(count == 0):
+    elif(count == 4):
+        b_x, b_y = x, y
         x, y = 715, 136
-    if(count == 0):
+    elif(count == 5):
+        b_x, b_y = x, y
         x, y = 316, 225
-    if(count == 0):
+    elif(count == 6):
+        b_x, b_y = x, y
         x, y = 510, 92
-    if(count == 0):
+    elif(count == 7):
+        b_x, b_y = x, y
         x, y = 692, 518
-    if(count == 0):
+    elif(count == 8):
+        b_x, b_y = x, y
         x, y = 682, 336
-    if(count == 0):
+    elif(count == 9):
+        b_x, b_y = x, y
         x, y = 712, 349
 
-    pass
+frame = 0
 
-def move_charcter(x, y):
+def move_charcter():
 
     clear_canvas()
-    #character.clip_draw(frame * 100, 100, 100, 100, x, 90)
+    if(b_x < x):
+        frame = 100
+    elif(b_x > x):
+        frame = 0
+    character.clip_draw(frame * 100, 100, 100, 100, x, 90)
 
     delay(0.05)
-    pass
+
 
 
 while True:
     save_coord()
     move_charcter(x, y)
+    count += 1
+    if(count == 10):
+        count = 0
 
 
     
