2188fcafc7dfcc49aae3d0ada880d4ac0aafe7dd
장우석 <cyj2651@naver.com>
2018-09-17 16:19:02 +0900

    drill 05

--- Drills/Drill_05/drill_05.py
+++ Drills/Drill_05/drill_05.py
@@ -1,16 +1,49 @@
 from pico2d import *
-import math
+
 
 open_canvas()
 
 grass = load_image('grass.png')
 character = load_image('character.png')
 
- def make_rectangle():
+
+
+def move_from_center_to_right():
+    x, y = 800 // 2 , 90
+    while x < 800 - 25:
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(x, y)
+        x += 20
+        delay(0.2)
+
+def move_up():
     pass
 
- def make_circle():
+def move_left():
     pass
+
+def move_right():
+    pass
+
+def move_down():
+    pass
+
+def move_from_left_to_center():
+    pass
+
+def make_rectangle():
+    #move_from_center_to_right()
+    move_up()
+    move_left()
+    move_down()
+    move_right()
+    move_from_left_to_center()
+
+
+def make_circle():
+    pass
+
 
 while True:
     make_rectangle()
