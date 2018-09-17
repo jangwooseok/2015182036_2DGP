14f4b9c155dd26edd11867a3adf3810f415fed8c
장우석 <cyj2651@naver.com>
2018-09-17 17:05:34 +0900

    drill_05 초기화 함수 완성
    10가지 값을 넣어줬다

--- Drills/Drill_05/drill_05.py
+++ Drills/Drill_05/drill_05.py
@@ -5,12 +5,24 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
-# fill here
+# 1
 
 def save_coord():
+    x0, y0 = 203, 535
+    x1, y1 = 132, 243
+    x2, y2 = 535, 470
+    x3, y3 = 477, 203
+    x4, y4 = 715, 136
+    x5, y5 = 316, 225
+    x6, y6 = 510, 92
+    x7, y7 = 692, 518
+    x8, y8 = 682, 336
+    x9, y9 = 712, 349
+
     pass
 
-def move_charcter():
+def move_charcter(x, y):
+    
     pass
 
 while True:
